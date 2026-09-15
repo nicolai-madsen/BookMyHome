0c82aff5c2fba5a2bdc1194cebc85c1305ff46f2
Sheephurrdurr <nicolai.hoiberg@hotmail.com>
2026-09-15 21:10:28 +0200

    Added api project

---
+++ Application/Controllers/AccommodationController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Persistence.Repositories;
+
+namespace Application.Controllers
+{
+    [ApiController]
+    [Route("api/accommodations")]
+    public class AccommodationController : ControllerBase
+    {
+        private readonly IAccommodationRepository _repository;
+
+        public AccommodationController(IAccommodationRepository repository)
+        {
+            _repository = repository;
+        }
+    }
+}
